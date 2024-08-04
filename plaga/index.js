b92e5f320eec72aec994dc365c86588bbb681521
plaguee <120738105+plaguee@users.noreply.github.com>
2024-08-04 21:47:20 +0200

    Add files via upload

---
+++ plaga/index.js
@@ -0,0 +1,6 @@
+function hideNav() {
+    document.getElementById('navbar').style.visibility = "hidden";
+}
+function openNav() {
+    document.getElementById('navbar').style.visibility = "visible";
+}
